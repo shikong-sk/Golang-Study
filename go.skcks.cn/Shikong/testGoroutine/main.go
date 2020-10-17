e1191f63bc14208f7c1e7fbc8db93899222182f8
shikong-sk <shikong-sk@users.noreply.github.com>
2020-10-18 02:48:21 +0800

    Update main.go

--- go.skcks.cn/Shikong/testGoroutine/main.go
+++ go.skcks.cn/Shikong/testGoroutine/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 )
 
+// 计数器
 var wg sync.WaitGroup
+
+var channelData chan string
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
@@ -16,6 +20,21 @@ func main() {
 		go randDelay(i)
 	}
 	wg.Wait()
+
+	//
+	//for i := 0; i < 10; i++ {
+	//	wg.Add(1)
+	//	go getChannelData()
+	//	go randDelayWithChannel(i)
+	//}
+	//wg.Wait()
+}
+
+func getChannelData(){
+	data := make([]string, 10)
+	x := <-channelData
+	fmt.Println(x)
+	fmt.Println(data)
 }
 
 func randDelay(i int) {
@@ -23,4 +42,11 @@ func randDelay(i int) {
 	delay := rand.Intn(1000-500) + 500
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 	fmt.Println(i, delay)
+}
+
+func randDelayWithChannel(i int) {
+	defer wg.Done()
+	delay := rand.Intn(1000-500) + 500
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	channelData <- strconv.Itoa(i) + "\t" + strconv.Itoa(delay)
 }
