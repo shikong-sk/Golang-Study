a84b377a457d8411c0cbd05723acedbf2fca2779
shikong-sk <shikong-sk@users.noreply.github.com>
2020-10-19 12:01:07 +0800

    Update checkServer.go

--- go.skcks.cn/Shikong/check-alive/checkServer.go
+++ go.skcks.cn/Shikong/check-alive/checkServer.go
@@ -5,60 +5,60 @@ import (
 	"check-alive/notification"
 	"check-alive/printJson"
 	"fmt"
-	"strconv"
 	"sync"
 	"time"
 )
 
 var wg sync.WaitGroup
 
+type api struct {
+	address string
+	alive   bool
+}
+
 func main() {
 	result := make(chan string, 4)
 	_ = sync.Map{}
 	apiMap := new(sync.Map)
-	apiMap.Store("http://192.168.1.241:1080/ncda/v2/api-docs", false)
-	apiMap.Store("http://192.168.1.241:1089/ncdk/v2/api-docs",false)
-	apiMap.Store("http://192.168.2.5:8010/ncda/v2/api-docs",false)
-	apiMap.Store("http://192.168.2.3:7010/ncdk/v2/api-docs",false)
+	apiMap.Store("241 Ncda 服务", api{address: "http://192.168.1.241:1080/ncda/v2/api-docs"})
+	apiMap.Store("241 Ncdk 服务", api{address: "http://192.168.1.241:1089/ncdk/v2/api-docs"})
+	apiMap.Store("测试 192.168.2.5 Ncda 服务", api{address: "http://192.168.2.5:8010/ncda/v2/api-docs"})
+	apiMap.Store("测试 192.168.2.3 Ncdk 服务", api{address: "http://192.168.2.3:7010/ncdk/v2/api-docs"})
 
 	processNum := 0
-	apiMap.Range(func(k,v interface{})bool{
+	apiMap.Range(func(k, v interface{}) bool {
 		processNum++
 		return true
 	})
 
-	api := map[string]bool{
-		"http://192.168.1.241:1080/ncda/v2/api-docs": false,
-		"http://192.168.1.241:1089/ncdk/v2/api-docs": false,
-		"http://192.168.2.5:8010/ncda/v2/api-docs":   false,
-		"http://192.168.2.3:7010/ncdk/v2/api-docs":   false,
-	}
-
 	for {
-		fmt.Println(time.Now())
-		wg.Add(len(api))
-		for k := range api {
-			go process(k, 1, result, api,k)
-		}
+		wg.Add(processNum)
+		apiMap.Range(func(k, v interface{}) bool {
+			go process(v.(api).address, 1, result, apiMap, k.(string))
+			return true
+		})
 		wg.Wait()
 		for i := 0; i < cap(result); i++ {
 			fmt.Println(<-result)
 		}
-		fmt.Println()
 		time.Sleep(time.Second)
 	}
 }
 
-func process(url string, timeout int32, result chan<- string,api map[string]bool,key string) {
+func process(url string, timeout int32, result chan<- string, apiMap *sync.Map, key string) {
 	defer wg.Done()
 	alive := CheckAlive(url, timeout)
-	if alive != api[key]{
-		api[key] = alive
+	store, _ := apiMap.Load(key)
+	if alive != store.(api).alive {
+		apiMap.Store(key, api{url, alive})
 		if alive {
-			_ = notification.Notification(url+"已上线", time.Now().Format("2006-01-02 15:04:05.000"))
+			result <- key + " 已上线" + time.Now().Format("2006-01-02 15:04:05.000") + "\n"
+			_ = notification.Notification(key+" 已上线", time.Now().Format("2006-01-02 15:04:05.000"))
+		} else {
+			result <- key + " 无法访问" + time.Now().Format("2006-01-02 15:04:05.000") + "\n"
+			_ = notification.Notification(key+" 无法访问", time.Now().Format("2006-01-02 15:04:05.000"))
 		}
 	}
-	result <- url + " => " + strconv.FormatBool(alive)
 }
 
 func CheckAlive(url string, timeout int32) bool {
